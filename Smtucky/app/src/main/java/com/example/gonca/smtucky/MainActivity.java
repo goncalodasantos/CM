8647aa1ec580a063fadab85ad98648f4636c9e89
InesFidalgo <ana_fidalgo18@hotmail.com>
2017-12-26 19:28:17 +0000

    fixes

--- Smtucky/app/src/main/java/com/example/gonca/smtucky/MainActivity.java
+++ Smtucky/app/src/main/java/com/example/gonca/smtucky/MainActivity.java
@@ -162,30 +162,22 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        setupTablayout();
-        final ViewPager viewpager = (ViewPager) findViewById(R.id.sliding_tabs);
+
+        setupRecycler();
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
         TabsAdapter adapter = new TabsAdapter(getSupportFragmentManager());
 
-        viewpager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewpager);
-        setupRecycler();
-        /*
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewpager.setCurrentItem(tab.getPosition());
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });*/
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
-
 
 
     private void setupTablayout(){
--- Smtucky/app/src/main/java/com/example/gonca/smtucky/Tab.java
+++ Smtucky/app/src/main/java/com/example/gonca/smtucky/Tab.java
@@ -36,9 +36,9 @@ public class Tab extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView=inflater.inflate(R.layout.tabs, container, false);
+        View rootView = inflater.inflate(R.layout.activity_route, container, false);
 
-        return(rootView);
+        return rootView;
     }
 
     public void onButtonPressed(Uri uri) {
