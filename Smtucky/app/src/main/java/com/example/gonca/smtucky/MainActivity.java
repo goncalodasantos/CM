c05db8612d742a20deaac3a2f689422e98fbfe61
Filipe Mendes <filipemendes1994@gmail.com>
2017-12-26 03:04:10 +0000

    FB Login done

--- Smtucky/app/src/main/java/com/example/gonca/smtucky/MainActivity.java
+++ Smtucky/app/src/main/java/com/example/gonca/smtucky/MainActivity.java
@@ -11,12 +11,18 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.login.Login;
+import com.facebook.login.LoginManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -47,8 +53,12 @@ public class MainActivity extends Activity {
         intent.putExtra("routenumber","6");
 
         startService(intent);//not startActivity!
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         setupRecycler();
     }
@@ -72,9 +82,32 @@ public class MainActivity extends Activity {
                 new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                LoginManager.getInstance().logOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                MainActivity.this.startActivity(intent);
+                return true;
 
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
 
+        }
+    }
 }
 
 
